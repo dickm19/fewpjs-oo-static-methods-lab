0dde1cdd7e8dcaf519b8bfe0fee88db6e9d003a1
dickm19 <maryclairedick@gmail.com>
2020-11-05 17:48:07 -0500

    this was hard wow lol

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^0-9a-zA-Z_'' -]/gi , '')
+  }
+
+  static titleize(string){
+  
+    const ignore = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    
+    let array = string.split(' ')
+    let newArray = []
+
+     array.forEach(word =>{
+      if (!ignore.includes(word) || array[0] == word){
+        newArray.push(this.capitalize(word))
+      }else{
+        newArray.push(word)
+      }
+    })
+    return newArray.join(" ")
+    
+   
+
+  }
 }
